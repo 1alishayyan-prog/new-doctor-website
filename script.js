4648ee430f2c0f8b1bdc193f1e079f6a8d62d43b
Dr.ALI SHAYYAN <1alishayyan@gmail.com>
2025-08-15 18:56:01 +0500

    speed up animations

--- script.js
+++ script.js
@@ -24,7 +24,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 observer.unobserve(entry.target);
             }
         });
-    }, { root: null, rootMargin: '0px 0px 0px 200px', threshold: 0 });
+    }, { root: null, 
+         // This makes the animation start 500px before the element is visible
+    rootMargin: '0px 0px 500px 0px', 
+    threshold: 0  });
 
     // Apply the observer to all elements with the .fade-in or .bar-item class
     document.querySelectorAll('.fade-in, .bar-item').forEach((el, index) => {
